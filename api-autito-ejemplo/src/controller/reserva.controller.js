23a0df3c081711b62cfd49f7550cd18c5b7eecb6
AlanUribe2000 <alanuribe2000@gmail.com>
2023-06-13 21:19:52 -0300

    Modificaciones, horarios eliminados y modificacion en get reserva

--- api-autito-ejemplo/src/controller/reserva.controller.js
+++ api-autito-ejemplo/src/controller/reserva.controller.js
@@ -1,20 +1,31 @@
 const pool = require('../config/db');
 
-const getReservas = async (req,res)  => {
-    const response = await pool.query('SELECT * FROM reservas')
+const getReservasDeporte = async (req,res)  => {
+    const {id_espacio} = req.body;
+    let date = new Date()
+    let day = date.getDate()
+    let month = date.getMonth()
+    let year = date.getFullYear()
+    if(month < 10){
+        month = "0"+month.toString()
+    }
+    let fechaActual = day.toString()+"-"+month.toString()+"-"+year.toString()
+    const response = await pool.query('SELECT * FROM reservas WHERE id_espacio = $1 AND fecha_fin >= $2',[id_espacio,fechaActual])
     res.status(200).json(response.rows);
 }
 
-const setReserva = async (req,res)  => {
-    const {id_usuario,id_espacio,fecha_fin,fecha_inicio} = req.body;
-    const response = await pool.query('INSERT INTO reservas (id_usuario,id_espacio,fecha_fin,fecha_inicio) VALUES ($1, $2, $3, $4)',[id_usuario,id_espacio,fecha_fin,fecha_inicio]);
+//luego me manda el dia y el horario
+
+const setReservaDeporte = async (req,res)  => {
+    const {id_usuario,id_espacio,fecha_fin,fecha_inicio,hora_inicio,hora_fin} = req.body;
+    const response = await pool.query('INSERT INTO reservas (id_usuario,id_espacio,fecha_fin,fecha_inicio,hora_inicio,hora_fin) VALUES ($1, $2, $3, $4, $5, $6)',[id_usuario,id_espacio,fecha_fin,fecha_inicio,hora_inicio,hora_fin]);
     res.status(200).json({
         message:'empresa agregada correctamente',
         body:{
-            reserva:{id_usuario,id_espacio,fecha_fin,fecha_inicio}
+            reserva:{id_usuario,id_espacio,fecha_fin,fecha_inicio,hora_inicio,hora_fin}
         }
     });    
     console.log(req);
 }
 
-module.exports = {getReservas,setReserva}
+module.exports = {getReservasDeporte,setReservaDeporte}
--- api-autito-ejemplo/src/routes/index.js
+++ api-autito-ejemplo/src/routes/index.js
@@ -4,19 +4,15 @@ const { getUsers, findOne, setUser} = require('../controller/usuario.controller'
 const { getTiposDeportes, getTiposDepartamentos, getTiposSalones, getTipos, setTipo} = require('../controller/tipoespacio.controller');
 const { getEmpresas,setEmpresa, getEmpresasDeportes, getEmpresasSalones,getEmpresasDepartamentos} = require('../controller/empresaController');
 const { getEspacios,setEspacio, getEspacioDeportes, getEspacioFindOne} = require('../controller/espacio.controller');
-const { getHorarios,setHorario} = require('../controller/horarios.controller');
-const { getReservas,setReserva} = require('../controller/reserva.controller');
+const { getReservasDeporte,setReservaDeporte} = require('../controller/reserva.controller');
 const { getServicios,setServicio} = require('../controller/servicio.controller');
 const { getEspacioServicios,setEspacioServicio,espacioServicios,servicioEspacios} = require('../controller/espacioservicio.controller');
-const { getReservasHorarios,setReservaHorario,reservaHorarios,horarioReservas} = require('../controller/reservashorarios.controller');
 //Getters
 router.get('/empresas', getEmpresas);
 router.get('/espacio', getEspacios);
-router.get('/horarios', getHorarios);
-router.get('/reservas', getReservas);
+router.get('/reservas', getReservasDeporte);
 router.get('/servicios', getServicios);
 router.get('/espacioservicio', getEspacioServicios);
-router.get('/reservashorarios', getReservasHorarios);
 
 //routes get usuarios
 router.get('/usuarios', getUsers);
@@ -40,19 +36,15 @@ router.get('/empresasDepartamentos', getEmpresasDepartamentos);
 //SETTERS
 router.post('/setEmpresa', setEmpresa);
 router.post('/setEspacio', setEspacio);
-router.post('/setHorario', setHorario);
-router.post('/setReserva', setReserva);
+router.post('/setReserva', setReservaDeporte);
 router.post('/setServicio', setServicio);
 router.post('/setEspacioServicio', setEspacioServicio);
-router.post('/setReservaHorario', setReservaHorario);
 
 router.post('/setTipo', setTipo);
 router.post('/setUser', setUser);
 
 router.post('/espacioServicios', espacioServicios); //A partir del id de un espacio devuelve los servicios del mismo
 router.post('/servicioEspacios', servicioEspacios); //A partir del id de un servicio devuelve los espacios que tienen ese servicio
-router.post('/reservaHorarios', reservaHorarios); //A partir del id de una reserva devuelve los horarios del mismo
-router.post('/horarioReservas', horarioReservas); //A partir del id de un horario devuelve las reservas que estén en ese horario
 
 router.post('/getEspacioFindOne', getEspacioFindOne); //A partir del id de la empresa devuelve el espacio
 
