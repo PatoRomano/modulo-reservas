0507625c962ed7a5c466271ebf18585dcfb357be
AlanUribe2000 <alanuribe2000@gmail.com>
2023-06-19 00:30:51 -0300

    Se agregó descripción a los espacios

--- api-autito-ejemplo/src/controller/espacio.controller.js
+++ api-autito-ejemplo/src/controller/espacio.controller.js
@@ -8,7 +8,7 @@ const getEspacios = async (req,res)  => {
 const getEspacioFindOne = async (req,res)  => {
     const {id_empresa} = req.body;
     const response = await pool.query('SELECT es.id, es.nombre as nombreespacio, es.precio_hora, '
-    +'es.hora_inicio, es.hora_fin, te.nombre as tipo FROM espacios es '
+    +'es.hora_inicio, es.hora_fin, es.descripcion te.nombre as tipo FROM espacios es '
     +'INNER JOIN tipo_espacio as te ON te.id = es.id_tipo '
     +'where te.id_padre = 1 and id_empresa  = $1',[id_empresa]);
     res.status(200).json(response.rows);
@@ -17,7 +17,7 @@ const getEspacioFindOne = async (req,res)  => {
 const getEspacioFindOneSalones = async (req,res)  => {
     const {id_empresa} = req.body;
     const response = await pool.query('SELECT es.id, es.nombre as nombreespacio, es.precio_hora, '
-    +'es.hora_inicio, es.hora_fin, te.nombre as tipo FROM espacios es '
+    +'es.hora_inicio, es.hora_fin, es.descripcion, te.nombre as tipo FROM espacios es '
     +'INNER JOIN tipo_espacio as te ON te.id = es.id_tipo '
     +'where te.id_padre = 3 and id_empresa  = $1',[id_empresa]);
     res.status(200).json(response.rows);
@@ -26,7 +26,7 @@ const getEspacioFindOneSalones = async (req,res)  => {
 const getEspacioFindOneDepartamentos = async (req,res)  => {
     const {id_empresa} = req.body;
     const response = await pool.query('SELECT es.id, es.nombre as nombreespacio, es.precio_hora, '
-    +'es.hora_inicio, es.hora_fin, te.nombre as tipo FROM espacios es '
+    +'es.hora_inicio, es.hora_fin, es.descripcion, te.nombre as tipo FROM espacios es '
     +'INNER JOIN tipo_espacio as te ON te.id = es.id_tipo '
     +'where te.id_padre = 2 and id_empresa  = $1',[id_empresa]);
     res.status(200).json(response.rows);
@@ -34,7 +34,7 @@ const getEspacioFindOneDepartamentos = async (req,res)  => {
 
 
 const getEspacioDeportes = async (req, res) => {
-    const query = 'SELECT e.nombre as espacio, tp.nombre as tipo, tp.capacidad, e.precio_hora as precio  FROM espacios e '+
+    const query = 'SELECT e.nombre as espacio, tp.nombre as tipo, tp.capacidad, e.precio_hora as precio, e.descripcion  FROM espacios e '+
     'INNER JOIN tipo_espacio tp on tp.id = e.id_tipo where tp.id_padre = 1';
     try {
         const response = await pool.query(query)
@@ -45,7 +45,7 @@ const getEspacioDeportes = async (req, res) => {
 }
 
 const getEspacioSalones = async (req, res) => {
-    const query = 'SELECT e.nombre as espacio, tp.nombre as tipo, tp.capacidad, e.precio_hora as precio  FROM espacios e '+
+    const query = 'SELECT e.nombre as espacio, tp.nombre as tipo, tp.capacidad, e.precio_hora as precio, e.descripcion FROM espacios e '+
     'INNER JOIN tipo_espacio tp on tp.id = e.id_tipo where tp.id_padre = 3';
     try {
         const response = await pool.query(query)
@@ -56,12 +56,12 @@ const getEspacioSalones = async (req, res) => {
 }
 
 const setEspacio = async (req,res)  => {
-    const {id_empresa,id_tipo,id_estado,nombre,precio_hora,hora_inicio,hora_fin} = req.body;
-    const response = await pool.query('INSERT INTO espacios (id_empresa,id_tipo,nombre,precio_hora,hora_inicio,hora_fin) VALUES ($1, $2, $3, $4, $5, $6)',[id_empresa,id_tipo,nombre,precio_hora,hora_inicio,hora_fin]);
+    const {id_empresa,id_tipo,id_estado,nombre,precio_hora,hora_inicio,hora_fin,descripcion} = req.body;
+    const response = await pool.query('INSERT INTO espacios (id_empresa,id_tipo,nombre,precio_hora,hora_inicio,hora_fin,descripcion) VALUES ($1, $2, $3, $4, $5, $6, %7)',[id_empresa,id_tipo,nombre,precio_hora,hora_inicio,hora_fin,descripcion]);
     res.status(200).json({
         message:'espacio agregado correctamente',
         body:{
-            espacio:{id_empresa,id_tipo,nombre,precio_hora,hora_inicio,hora_fin}
+            espacio:{id_empresa,id_tipo,nombre,precio_hora,hora_inicio,hora_fin,descripcion}
         }
     });    
     console.log(req);
